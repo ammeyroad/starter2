e1bdcb1f55efcd83730f4afc2710150c37e364fc
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2025-08-25 04:09:13 +0700

    feat: example usage of Nuxt Content 3 in home page

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{ doc.title }}</h1>
+    <p>{{ doc.description }}</p>
+    <ContentRenderer :value="doc" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data: doc } = await useAsyncData('homepage', () =>
+  queryContent('/').findOne()
+)
+</script>
